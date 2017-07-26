333527d7b8659aea01aae84be9f2c0c062cf0802
aaroth50 <aaronkroth@yahoo.com>
2017-07-25 16:14:31 -1000

    Swapped color

--- scripts/stereoanaglyph.py
+++ scripts/stereoanaglyph.py
@@ -14,10 +14,10 @@ def Anaglyph(imgl, imgr):
 	rightMap = right.load()
 
 	left = grayscale(left)
-	left = colorize(left, (0,0,0),(255,0,0))
+	left = colorize(left, (0,0,0),(0,255,255))
 
 	right = grayscale(right)
-	right = colorize(right, (0,0,0),(0,255,255))
+	right = colorize(right, (0,0,0),(255,0,0))
 
 	anagimg = Image.blend(left,right,0.5)
 
