10424f569c81a18ec39c043d6175e056848295b0
aaroth50 <aaronkroth@yahoo.com>
2017-07-27 14:18:33 -1000

    added argument for parallel and toedin

--- scripts/stereoanaglyph.py
+++ scripts/stereoanaglyph.py
@@ -6,27 +6,37 @@ from PIL import ImageOps
 from PIL.ImageOps import grayscale
 from PIL.ImageOps import colorize
 
-def Anaglyph(imgl, imgr):
+def Anaglyph(imgl, imgr, angle):
 	right = imgr
 	left = imgl
 	width, height = left.size
 	leftMap = left.load()
 	rightMap = right.load()
 
-	left = grayscale(left)
-	left = colorize(left, (0,0,0),(0,255,255))
+	if angle == 'parallel':
+		left = grayscale(left)
+		left = colorize(left, (0,0,0),(0,255,255))
 
-	right = grayscale(right)
-	right = colorize(right, (0,0,0),(255,0,0))
+		right = grayscale(right)
+		right = colorize(right, (0,0,0),(255,0,0))
+
+	if angle == 'toedin':
+		left = grayscale(left)
+		left = colorize(left, (0,0,0),(255,0,0))
+
+		right = grayscale(right)
+		right = colorize(right, (0,0,0),(0,255,255))
 
 	anagimg = Image.blend(left,right,0.5)
-
+	#anagimg = ImageChops.add(right,left,2)
 	return anagimg
 
 if __name__ == '__main__':
     imgind = 0
     imgind1 = 0
+    imgind2 = 0
     if len(sys.argv) > 1:
         imgind = sys.argv[1]
         imgind1 = sys.argv[2]
-        Anaglyph(imgind, imgind1)
+        imgind2 = sys.argv[3]
+        Anaglyph(imgind, imgind1, imgind2)
